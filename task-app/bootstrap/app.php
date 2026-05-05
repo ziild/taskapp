f1c7a12be35b8dea0f2efd0606e70e9b3d56c5ab
ziild <blazezildjan123@example.com>
2026-05-05 22:20:27 +0800

    railway

--- task-app/bootstrap/app.php
+++ task-app/bootstrap/app.php
@@ -10,9 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
